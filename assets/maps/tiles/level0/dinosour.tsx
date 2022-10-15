3328fc06e6678165effdee6f4a87c2999af1ccee
LodurBlin <masyanasim@gmail.com>
2022-10-15 14:09:46 +0300

    Аллилуйя

---
+++ assets/maps/tiles/level0/dinosour.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="bird" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="../../pngs/level0/Bird.png" width="172" height="60"/>
+</tileset>
---
+++ assets/maps/tiles/player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="player" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../images/Nick Names.png" width="96" height="102"/>
+</tileset>
